ab899ee4bf30985747f3c91c5c748146026bbb38
Ramani <Santosh_Ramani@infosys.com>
2016-08-29 11:50:24 +0530

    RDKB-7258: Log MoCA Enable value and Status in the Armconsole
    Reason for change: Check for moca enable value in moca_status.sh script
    Test Procedure: Tested on target by checking the ArmConsoleLog.txt.0 file
    Risks: None
    Signed-off-by: Santosh_Ramani@infosys.com
    
    Change-Id: I27f0a964c0884700784df1e1094d1177e212d90e

--- arch/intel_usg/boards/arm_shared/scripts/moca_status.sh
+++ arch/intel_usg/boards/arm_shared/scripts/moca_status.sh
@@ -15,3 +15,10 @@ source /etc/utopia/service.d/log_capture_path.sh
 	moca_status=`dmcli eRT getv Device.MoCA.Interface.1.Status | grep string | awk '{print $5}'`
 	echo_t "MoCA is $moca_status"
 #done
+        echo_t "Checking for MoCA Enable.."
+	moca_enable=`dmcli eRT getv Device.MoCA.Interface.1.Enable | grep bool | awk '{print $5}'`
+	if [ $moca_enable == "true" ];then 
+        echo_t "MoCA is Enabled"
+        else 
+        echo_t "MoCA is Disabled"
+        fi
