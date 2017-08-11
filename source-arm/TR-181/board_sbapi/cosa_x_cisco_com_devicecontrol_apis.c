0eff32f52c692f5321041bbacdad85470c07a2ce
Arti Katre <arti.katre@broadcom.com>
2017-08-11 16:27:16 -0700

    TCCBR-1595: Country Code is US/888 on V2.1 Hardware
    Reason for change: Added the change to remove the file for fixing it by factory reset in case somebody downgrades the firmware.
    Test Procedure: verify working on the target board.
    Risks: Low
    Signed-off-by: arti.katre@broadcom.com
    
    Change-Id: I42856daef1d46893350367bf2e425667c89f1942

--- source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
@@ -1668,6 +1668,7 @@ void restoreAllDBs()
 #endif
 #if defined (_CBR_PRODUCT_REQ_)
 	system("rm -f /data/nvram");
+	system("rm -f /data/country_code_*");
 	system("touch /tmp/wifi_factory_reset");
         /* Remove maintenance window data on factory reset */
         system("rm -f /nvram/.FirmwareUpgradeEndTime");
