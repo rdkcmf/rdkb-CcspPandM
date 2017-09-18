9d3fc269aad32eb55381a6e69d2dec5ca3b6d688
Sudharsan Raman <sudharsan_raman@comcast.com>
2017-09-18 17:24:29 +0530

    RDKB-13815 RDKB-14216 RDKB-13932 TR181 implementation for enable/disable
    Reason for change:Scheduler should be configured in XB3 to trigger Xconf firmware check.
    Time to be scheduled will be available from the Xconf. This will be done by the TR181
    parameter "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.PeriodicFWCheck"
    Test Procedure:After flashing the image and reboot, on ARM side check "crontab -l" and see the
    xb3_firmwareDwnld cron job. Whenever there is a change in time from the server for firmware
    download, the cronjob for  "xb3_firmwareDwnld" should automatically change. Use the TR181
    parameter for this.
    Risks: None
    Signed-off-by: Sudharsan Raman <sudharsan_raman@comcast.com>
    
    Change-Id: Idd939ce5561bb6d1b7b7aa7c8d346b55cb1b87f6

--- source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
+++ source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
@@ -4167,6 +4167,130 @@ Feature_SetParamBoolValue
     }
     return FALSE;
 }
+
+/**********************************************************************  
+
+    caller:     owner of this object 
+
+    prototype: 
+
+        BOOL
+        PeriodicFWCheck_GetParamBoolValue
+            (
+                ANSC_HANDLE                 hInsContext,
+                char*                       ParamName,
+                BOOL*                       pBool
+            );
+
+    description:
+
+        This function is called to retrieve Boolean parameter value; 
+
+    argument:   ANSC_HANDLE                 hInsContext,
+                The instance handle;
+
+                char*                       ParamName,
+                The parameter name;
+
+                BOOL*                       pBool
+                The buffer of returned boolean value;
+
+    return:     TRUE if succeeded.
+
+**********************************************************************/
+
+BOOL
+PeriodicFWCheck_GetParamBoolValue
+
+    (
+        ANSC_HANDLE                 hInsContext,
+        char*                       ParamName,
+        BOOL*                       pBool
+    )
+{
+    /* check the parameter name and return the corresponding value */
+
+	if( AnscEqualString(ParamName, "Enable", TRUE))
+		{
+			/* collect value */
+			char buf[8];
+			syscfg_get( NULL, "PeriodicFWCheck_Enable", buf, sizeof(buf));
+	
+			if( buf != NULL )
+			{
+				if (strcmp(buf, "true") == 0)
+					*pBool = TRUE;
+				else
+					*pBool = FALSE;
+			}
+			return TRUE;
+		}
+
+    return FALSE;
+}
+
+
+/**********************************************************************  
+
+    caller:     owner of this object 
+
+    prototype: 
+
+        BOOL
+        PeriodicFWCheck_SetParamBoolValue
+            (
+                ANSC_HANDLE                 hInsContext,
+                char*                       ParamName,
+                BOOL                        bValue
+            );
+
+    description:
+
+        This function is called to set BOOL parameter value; 
+
+    argument:   ANSC_HANDLE                 hInsContext,
+                The instance handle;
+
+                char*                       ParamName,
+                The parameter name;
+
+                BOOL                        bValue
+                The updated BOOL value;
+
+    return:     TRUE if succeeded.
+
+**********************************************************************/
+BOOL
+PeriodicFWCheck_SetParamBoolValue
+
+    (
+        ANSC_HANDLE                 hInsContext,
+        char*                       ParamName,
+        BOOL                        bValue
+    )
+{
+ 	    if( AnscEqualString(ParamName, "Enable", TRUE))
+		{
+			/* collect value */
+			if( bValue == TRUE)
+			{
+					syscfg_set(NULL, "PeriodicFWCheck_Enable", "true");
+					syscfg_commit();
+					system("/etc/firmwareSched.sh &");
+			}
+			else
+			{
+					syscfg_set(NULL, "PeriodicFWCheck_Enable", "false");
+					syscfg_commit();
+	
+					system("sh /etc/firmwareSched.sh RemoveCronJob");
+					system("killall -9 firmwareSched.sh");
+			}
+			return TRUE;
+		}	
+    return FALSE;
+}
+
 /**********************************************************************  
 
     caller:     owner of this object 
