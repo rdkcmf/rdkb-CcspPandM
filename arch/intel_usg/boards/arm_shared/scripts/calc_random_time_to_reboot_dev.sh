199123227b23df425f74c14d87146d8752fd8ef2
David Fang <david_fang@cable.comcast.com>
2018-05-04 03:42:29 +0000

    TCCBR-2704 : Remove ccsp-pandm-cbr.patch
    Reason for change: Remove patch and integrate changes into common code
    Test Procedure: None
    Risks: None
    
    Change-Id: Ib2aa4ca0b50bc9f5901c8ccec63bd17f9a4602be
    Signed-off-by: David Fang <david_fang@cable.comcast.com>

--- arch/intel_usg/boards/arm_shared/scripts/calc_random_time_to_reboot_dev.sh
+++ arch/intel_usg/boards/arm_shared/scripts/calc_random_time_to_reboot_dev.sh
@@ -19,13 +19,15 @@
 ################################################################################
 #
 source /etc/utopia/service.d/log_env_var.sh
+source /etc/device.properties
+
 SyseventdCrashed="/rdklogs/syseventd_crashed"
 calcRandTimetoReboot()
 {
     rand_hr=0
     rand_min=0
     rand_sec=0
-process_crashed_is=$1
+    process_crashed_is=$1
     # Calculate random min
     rand_min=`awk -v min=0 -v max=59 -v seed="$(date +%N)" 'BEGIN{srand(seed);print int(min+rand()*(max-min+1))}'`
 
@@ -45,5 +47,99 @@ process_crashed_is=$1
      /rdklogger/backupLogs.sh "true" "$process_crashed_is"
 }
 
-calcRandTimetoReboot $1
+calcRandTimetoReboot_CBR()
+{
+    process_crashed_is=$1
+
+    # Extract maintenance window start and end time
+    start_time=`dmcli eRT getv Device.DeviceInfo.X_RDKCENTRAL-COM_MaintenanceWindow.FirmwareUpgradeStartTime | grep "value:" | cut -d ":" -f 3 | tr -d ' '`
+    end_time=`dmcli eRT getv Device.DeviceInfo.X_RDKCENTRAL-COM_MaintenanceWindow.FirmwareUpgradeEndTime | grep "value:" | cut -d ":" -f 3 | tr -d ' '`
+
+    if [ "$start_time" -eq "$end_time" ]
+    then
+        echo "Start time can not be equal to end time"
+        echo "Resetting values to default"
+        dmcli eRT setv Device.DeviceInfo.X_RDKCENTRAL-COM_MaintenanceWindow.FirmwareUpgradeStartTime string "0"
+        dmcli eRT setv Device.DeviceInfo.X_RDKCENTRAL-COM_MaintenanceWindow.FirmwareUpgradeEndTime string "10800"
+        start_time=0
+        end_time=10800
+    fi
+
+    echo "Firmware upgrade start time : $start_time"
+    echo "Firmware upgrade end time : $end_time"
+
+    if [ "$start_time" -gt "$end_time" ]
+    then
+        start_time=$(($start_time-86400))
+    fi
+
+    #Calculate random value
+    random_time=`awk -v min=$start_time -v max=$end_time 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'`
+
+    if [ $random_time -le 0 ]
+    then
+        random_time=$((random_time+86400))
+    fi
+    random_time_in_sec=$random_time
+
+    # Calculate random second
+    rand_sec=$((random_time%60))
+
+    # Calculate random min
+    random_time=$((random_time/60))
+    rand_min=$((random_time%60))
+
+    # Calculate random hour
+    random_time=$((random_time/60))
+    rand_hr=$((random_time%60))
+
+    echo "Time Generated : $rand_hr hr $rand_min min $rand_sec sec"
+
+    # Get current time
+    if [ "$UTC_ENABLE" == "true" ]
+    then
+        cur_hr="10#"`LTime H`
+        cur_min="10#"`LTime M`
+        cur_sec="10#"`date +"%S"`
+    else
+        cur_hr="10#"`date +"%H"`
+        cur_min="10#"`date +"%M"`
+        cur_sec="10#"`date +"%S"`
+    fi
+
+    curr_hr_in_sec=$((cur_hr*60*60))
+    curr_min_in_sec=$((cur_min*60))
+    curr_time_in_sec=$((curr_hr_in_sec+curr_min_in_sec+cur_sec))
+
+    if [ $curr_time_in_sec -le $random_time_in_sec ]
+    then
+        sec_to_sleep=$((random_time_in_sec-curr_time_in_sec))
+    else
+        sec_to_12=$((86400-curr_time_in_sec))
+        sec_to_sleep=$((sec_to_12+random_time_in_sec))
+    fi
+
+    time=$(( `date +%s`+$sec_to_sleep ))
+    date_final=`date -d @${time} +"%T"`
+
+    echo "Action on $date_final"
+
+    echo "SLEEPING FOR $sec_to_sleep seconds"
+
+    sleep $sec_to_sleep
+    
+    echo "Got up after $sec_to_sleep seconds"  
+
+    touch $HAVECRASH
+    rm -rf $SyseventdCrashed
+    /rdklogger/backupLogs.sh "true" "$process_crashed_is"
+}
+
+#TCCBR-2704
+if [ "x$BOX_TYPE" == "xTCCBR" ]
+then
+    calcRandTimetoReboot_CBR $1
+else
+    calcRandTimetoReboot $1
+fi
 
