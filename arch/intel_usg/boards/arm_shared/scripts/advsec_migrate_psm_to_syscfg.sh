4619f816f3cd4f0aac67d7a5c372eae928e70e83
Amit <amit_ghorpade@comcast.com>
2018-05-16 23:00:48 +0000

    RDKB-17237:Refactor PSM to SysDB for DeviceFingerPrint.
    Reason for change:This change is bring migration from PSM
    to SysDB code just before start of PandM as DML (TR181)
    for DeviceFingerPrint is now managed by PandM.And reset
    any old setting in psm.
    
    Test Procedure:
    1. Use GW with PSM set(enabled) previously for DeviceFinger Print.
    2. New FW upgrade containing this change.
    3. Ensure DeviceFingerPrint Service is enabled on GW boot up.
    
    Change-Id: I2a503543c1dcbbeea2e3c70d627c880f1a630fcf
    Risks: Low
    Signed-off-by: Amit_Ghorpade@cable.comcast.com

--- arch/intel_usg/boards/arm_shared/scripts/advsec_migrate_psm_to_syscfg.sh
+++ arch/intel_usg/boards/arm_shared/scripts/advsec_migrate_psm_to_syscfg.sh
@@ -5,11 +5,33 @@
 PSM_DB=/nvram/bbhm_cur_cfg.xml
 ADVSEC_FP_RECORD="eRT.com.cisco.spvtg.ccsp.advsecurity.Device.DeviceInfo.X_RDKCENTRAL-COM_DeviceFingerPrint.Enable"
 ADVSEC_FP_SYSCFG=Advsecurity_DeviceFingerPrint
-if [ -f $PSM_DB ]; then
-	_res=`grep $ADVSEC_FP_RECORD $PSM_DB | grep "type=\"astr\">1</Record>"`
-	if [ "$?" = "0" ]; then
+
+check_psm_setting()
+{
+	if [ -f $PSM_DB ]; then
+		_res=`grep $ADVSEC_FP_RECORD $PSM_DB | grep "type=\"astr\">1</Record>"`
+		if [ "$?" = "0" ]; then
+			echo "true"
+		else
+			echo "false"
+		fi
+	fi
+}
+
+load_syscfg_setting()
+{
+	if [ "$( check_psm_setting )" = "true" ]; then
 		_res=`syscfg set $ADVSEC_FP_SYSCFG 1`
 		_res=`syscfg commit`
+	fi
+}
+
+reset_psm_setting()
+{
+	if [ "$( check_psm_setting )" = "true" ]; then
 		_res=`psmcli set eRT.com.cisco.spvtg.ccsp.advsecurity.Device.DeviceInfo.X_RDKCENTRAL-COM_DeviceFingerPrint.Enable 0`
 	fi
-fi
+}
+
+$1
+
