90bf19a005a11b336995edae81aaf446df26af94
Arathy Vikram <ArathyVikram_Nair@comcast.com>
2020-12-02 12:55:16 +0530

    RDKB-32899 : Optimization of log enable and log level to improve boot time
    Reason for change: To read log enable/level from syscfg db during bootup
    Test Procedure: As mentioned in ticket
    Risks: None
    Signed-off-by: ArathyVikram_Nair@comcast.com
    
    (cherry picked from commit 3eda6e55b39f2fa7b7730b2bf936397b38fb9b60)
    Change-Id: Ib434725459c0061e1767eb22fe9cccfba4196c7a

--- source/PandMSsp/ssp_main.c
+++ source/PandMSsp/ssp_main.c
@@ -678,12 +678,29 @@ if(id != 0)
 
     cmd_dispatch('e');
 
-	#ifndef DISABLE_LOGAGENT
-	RDKLogEnable = GetLogInfo(bus_handle,g_Subsystem,"Device.LogAgent.X_RDKCENTRAL-COM_LoggerEnable");
-	RDKLogLevel = (char)GetLogInfo(bus_handle,g_Subsystem,"Device.LogAgent.X_RDKCENTRAL-X_RDKCENTRAL-COM_LogLevel");
-	PAM_RDKLogLevel = GetLogInfo(bus_handle,g_Subsystem,"Device.LogAgent.X_RDKCENTRAL-COM_PAM_LogLevel");
-	PAM_RDKLogEnable = (char)GetLogInfo(bus_handle,g_Subsystem,"Device.LogAgent.X_RDKCENTRAL-COM_PAM_LoggerEnable");
-	#endif
+    syscfg_init();
+    CcspTraceInfo(("PAM_DBG:-------Read Log Info\n"));
+    char buffer[5] = {0};
+    if( 0 == syscfg_get( NULL, "X_RDKCENTRAL-COM_LoggerEnable" , buffer, sizeof( buffer ) ) &&  ( buffer[0] != '\0' ) )
+    {
+        RDKLogEnable = (BOOL)atoi(buffer);
+    }
+    memset(buffer, 0, sizeof(buffer));
+    if( 0 == syscfg_get( NULL, "X_RDKCENTRAL-COM_LogLevel" , buffer, sizeof( buffer ) ) &&  ( buffer[0] != '\0' ) )
+    {
+        RDKLogLevel = (ULONG )atoi(buffer);
+    }
+    memset(buffer, 0, sizeof(buffer));
+    if( 0 == syscfg_get( NULL, "X_RDKCENTRAL-COM_PAM_LogLevel" , buffer, sizeof( buffer ) ) &&  ( buffer[0] != '\0' ) )
+    {
+        PAM_RDKLogLevel = (ULONG)atoi(buffer);
+    }
+    memset(buffer, 0, sizeof(buffer));
+    if( 0 == syscfg_get( NULL, "X_RDKCENTRAL-COM_PAM_LoggerEnable" , buffer, sizeof( buffer ) ) &&  ( buffer[0] != '\0' ) )
+    {
+        PAM_RDKLogEnable = (BOOL)atoi(buffer);
+    }
+    CcspTraceInfo(("PAM_DBG:-------Log Info values RDKLogEnable:%d,RDKLogLevel:%u,PAM_RDKLogLevel:%u,PAM_RDKLogEnable:%d\n",RDKLogEnable,RDKLogLevel,PAM_RDKLogLevel, PAM_RDKLogEnable ));
     // printf("Calling Docsis\n");
 
     // ICC_init();
