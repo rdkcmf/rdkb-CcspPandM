24d5e5507fa5585ebc075fbf50381e3e809fc0b3
Andre McCurdy <armccurdy@gmail.com>
2021-04-26 19:23:15 -0700

    RDKCOM-2109 RDKBDEV-380: drop use of UINT8 data type
    Reason for change:
    drop use of UINT8 data type in KickstartTable_GetParamStringValue()
    The UINT8 data type is not defined within ccsp-p-and-m.
    Test Procedure: Build & Basic Sanity
    Risks: None
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    
    (cherry picked from commit f9829b6ad007d58b7b61fa6f79420b7e44c47f61)
    Change-Id: Ia48ccc8eac777e59a77d4a3af51036d455c908bf

--- source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
+++ source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
@@ -3759,7 +3759,7 @@ KickstartTable_GetParamStringValue
             {
                 if( pValue != NULL )
                 {
-                    bin2hexstring( pValue, (UINT8 *)pPtr, pKickstartTable->SecurityNumberLen );
+                    bin2hexstring( pValue, (unsigned char *) pPtr, pKickstartTable->SecurityNumberLen );
                     lRet = 0;
                 }
             }
